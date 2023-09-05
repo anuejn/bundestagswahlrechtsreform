295a5134bd4386b748e4ddac4594820cbd746752
Jaro Habiger <jarohabiger@googlemail.com>
2023-09-05 02:42:07 +0200

    revert dark mode

--- src/index.tsx
+++ src/index.tsx
@@ -3,29 +3,15 @@ import ReactDOM from 'react-dom';
 import 'tufte-css/tufte.css';
 import Essay from './essay.mdx';
 import { MDXProvider } from '@mdx-js/react';
-import { ThemeProvider, createTheme, useMediaQuery } from '@mui/material';
 
 window.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App />, document.getElementById('root'));
 });
 
 function App() {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: prefersDarkMode ? 'dark' : 'light',
-        },
-      }),
-    [prefersDarkMode]
-  );
-
   return (
-    <ThemeProvider theme={theme}>
-      <MDXProvider>
-        <Essay />
-      </MDXProvider>
-    </ThemeProvider>
+    <MDXProvider>
+      <Essay />
+    </MDXProvider>
   );
 }
