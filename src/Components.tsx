340bc11d645229225beb3864d28d5e2fbe117005
Jaro Habiger <jarohabiger@googlemail.com>
2023-09-05 19:55:07 +0200

    improve figure numbering

--- src/Components.tsx
+++ src/Components.tsx
@@ -27,18 +27,14 @@ export function SideNote({
 
 let fig_count = 1;
 export function Caption({ children }: { children: JSX.Element[] | JSX.Element }) {
-  const childrenList = Array.isArray(children) ? children : [children];
-  const modifiedChildren = childrenList.map((c, i) =>
-    i == 0 ? (
-      <p>
-        <b>
-          <i>Abbildung {fig_count++}:</i> {c.props.children}
-        </b>
-      </p>
-    ) : (
-      c
-    )
-  );
+  const childrenList = Array.isArray(children) ? [...children] : [children];
+  childrenList[0] = (
+    <p>
+      <b>
+        <i>Abbildung {fig_count++}:</i> {childrenList[0].props.children}
+      </b>
+    </p>
+  )
 
-  return <SideNote label={<>&#8853;</>}>{modifiedChildren}</SideNote>;
+  return <SideNote label={<>&#8853;</>}>{childrenList.map((x, i) => ({...x, key: i}))}</SideNote>;
 }
